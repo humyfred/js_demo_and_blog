a69aca04d61daca9f4d00375bd8abc52143a79b8
humyfred <2681776832@qq.com>
2017-04-12 15:13:13 +0800

    js深复制

---
+++ src/deepCopy/deepCopy.js
@@ -0,0 +1,32 @@
+function deepCopy(target, source){
+	var toString = Object.prototype.toString;
+	var cache = {};
+	target = toString.call(target) === '[object Object]' ? target : {};
+	cache[source] = target;//处理环
+	target = copy(target, source);
+ 
+	function copy(target, source){
+ 		for(var key in source){
+ 			if(source.hasOwnProperty(key)){
+ 			   if(typeof source[key] === 'object'){
+ 			   	 if(cache[source[key]]){
+ 			   	   target[key] = cache[source[key]];
+ 			   	   continue ;
+ 			   	 }
+ 			   	 var cp = toString.call(source[key]) === '[object Array]' ? []: {};
+ 			   	 target[key] = copy(cp, source[key]);
+ 			   	 cache[source[key]] = cp;
+ 			   }else{
+ 			     target[key] = source[key];
+ 			   }
+ 			}
+
+ 		}
+ 		return target;
+	}
+	return target;
+}
+
+
+//
+//
