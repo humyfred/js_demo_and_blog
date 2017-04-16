3fe4dac7fc790b8ef7c33479a96c78b01c23255f
humyfred <513407656@qq.com>
2017-04-17 00:19:23 +0800

    add fast click test

---
+++ src/fastclick/fastclick.js
@@ -0,0 +1,60 @@
+function fastclick(){
+
+	this.init();
+}
+
+fastclick.prototype.init = function(){
+	document.body.addEventListener('click', this.onClick, true);
+	document.body.addEventListener('touchstart', this.touchStart.bind(this), false);
+	document.body.addEventListener('touchmove', this.touchMove.bind(this), false);
+	document.body.addEventListener('touchend', this.touchEnd.bind(this), false);
+	//document.body.addEventListener('touchcancel', this.touchCancel.bind(this), false);
+
+}
+
+fastclick.prototype.sendClick = function(target, event){
+	var touch = event.changedTouches[0];
+	var clickEvent = document.createEvent('MouseEvents');
+	clickEvent.initMouseEvent('click', true, true, window, 1, touch.screenX, touch.screenY, touch.clientX, touch.clientY, false, false, false, false, 0, null);
+	clickEvent.forwardedTouchEvent = true;
+	target.dispatchEvent(clickEvent);
+}
+
+fastclick.prototype.touchStart = function(event){
+	if (event.targetTouches.length > 1) {//多指触控不做处理
+		return true;
+	}
+
+	this.target = event.target;
+
+}
+
+fastclick.prototype.touchMove = function(event){
+	if(this.target!== event.target){//移动了则取消触碰时最初元素
+		this.target = null;
+	}
+}
+
+fastclick.prototype.touchEnd = function(event){
+	if(!this.target){//移动则不派发click事件
+		return false;
+	}
+	event.preventDefault();
+	this.sendClick(this.target, event);
+}
+
+fastclick.prototype.touchCancel = function() {
+	this.target = null;
+};
+
+
+fastclick.prototype.onclick = function(evt) {
+	if(evt.forwardedTouchEvent){
+		return true;
+	}
+	if(this.target){
+		return true;
+	}
+	evt.preventDefault();
+	evt.stopPropagation();
+};
